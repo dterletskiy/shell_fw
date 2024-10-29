f98ace4aa353cee40a03833a82bdab8b1b997598
Dmitriy Terletskiy <daterletskiy@gmail.com>
2024-10-29 12:59:00 +0200

    CHanged 'print' implementation.
    In 'print' function since now is used 'frintf' instead oe 'echo'.
    
    Implemented 'print_text_in_bunner' function.
    
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- print.sh
+++ print.sh
@@ -1,11 +1,48 @@
+readonly SPLIT_ARGUMENTS=1
+
+# Building bunner data
+readonly DELIMITER_CHAR="-"
+readonly SIDE_CHAR="|"
+readonly SPACE_CHAR=" "
+
+readonly BANNER_LENGTH=200
+
+readonly DELIMITER_1="+"$(printf "%$(( BANNER_LENGTH - 2 ))s" | tr ' ' "${DELIMITER_CHAR}")"+"
+readonly DELIMITER_1_LENGTH=$(echo -n "${DELIMITER_1}" | wc -c)
+
+readonly DELIMITER_2="||"$(printf "%$(( BANNER_LENGTH - 4 ))s" | tr ' ' "${SPACE_CHAR}")"||"
+readonly DELIMITER_2_LENGTH=$(echo -n "${DELIMITER_2}" | wc -c)
+
+
+
+function print_text_in_bunner( )
+{
+   local TEXT=${1}
+   local TEXT_LENGTH=$(echo -n "${TEXT}" | wc -c)
+
+   local SIDE_SPACE_COUNT=$(( ( BANNER_LENGTH - TEXT_LENGTH - 2 ) / 2 ))
+   local SIDE_SPACE=$(printf "%${SIDE_SPACE_COUNT}s" | tr ' ' "${SPACE_CHAR}")
+   local SPACE_ADD_COUNT=$(( ( BANNER_LENGTH - TEXT_LENGTH - 2 ) % 2 ))
+   local SPACE_ADD=$(printf "%${SPACE_ADD_COUNT}s" | tr ' ' "${SPACE_CHAR}")
+
+   local BG_COLOR=${ECHO_BG_LightYellow}
+   local FG_COLOR=${ECHO_FG_Red}
+   local COLOR=${BG_COLOR}${FG_COLOR}
+   local COLOR_RESET=${ECHO_RESET}
+   local FORMAT=${COLOR}%s${COLOR_RESET}
+
+   printf "${FORMAT}\n" "${DELIMITER_1}"
+   printf "${FORMAT}\n" "${DELIMITER_2}"
+   printf "${FORMAT}\n" "${DELIMITER_2}"
+   printf "${FORMAT}\n" "${SIDE_CHAR}${SIDE_SPACE}${TEXT}${SIDE_SPACE}${SPACE_ADD}${SIDE_CHAR}"
+   printf "${FORMAT}\n" "${DELIMITER_2}"
+   printf "${FORMAT}\n" "${DELIMITER_2}"
+   printf "${FORMAT}\n" "${DELIMITER_1}"
+}
+
 function action_begin( )
 {
-   local LOCAL_MESSAGE=$@
-   echo -e ${ECHO_HEADER}
-   echo "/***********************"
-   echo " * ${LOCAL_MESSAGE}"
-   echo " **********************/"
-   echo -e ${ECHO_RESET}
+   print_text_in_bunner $@
    #press_any_key
 }
 function action_end( )
@@ -14,11 +51,26 @@ function action_end( )
    #press_any_key
 }
 
-function print( )
+function print_old( )
 {
    local LOCAL_FORMAT=$1
    local LOCAL_MESSAGE=("${!2}")
    echo -e ${LOCAL_FORMAT}${LOCAL_MESSAGE[@]}${ECHO_RESET}
+}
+
+function print( )
+{
+   local LOCAL_FORMAT=$1
+   local LOCAL_MESSAGE=("${!2}")
+
+   if [[ 0 -eq ${SPLIT_ARGUMENTS} ]]; then
+      # No split arguments
+      printf "${LOCAL_FORMAT}%s${ECHO_RESET}" ${LOCAL_MESSAGE[@]}
+      printf "\n"
+   else
+      # Split arguments
+      printf "${LOCAL_FORMAT}%s${ECHO_RESET}\n" "${LOCAL_MESSAGE[@]}"
+   fi
 }
 
 function print_and_run( )
