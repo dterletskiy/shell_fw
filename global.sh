db1aad2de745fd6ccd21d72c9ffa58abcc993c6e
Dmitriy Terletskiy <daterletskiy@gmail.com>
2025-01-30 19:03:54 +0200

    Fix 'global.sh' include.
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- global.sh
+++ global.sh
@@ -3,7 +3,7 @@ if [ -n "${__SFW_GLOBAL_SH__}" ]; then
 fi
 __SFW_GLOBAL_SH__=1
 
-source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/arg_parse.sh"
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/arg_pars.sh"
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/base.sh"
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/drive.sh"
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/git.sh"
