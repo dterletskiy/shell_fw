2881c425ab00e1368550df4ddaccb75c011b95cc
Dmitriy Terletskiy <daterletskiy@gmail.com>
2025-07-15 16:47:01 +0300

    parse_range function inmplemented.
    This function parses list of numbers and ranges of numbers passed in the string
    and fills an array of numbers passed as a second argument.
    List of numbers and ranges must be passed as a string and separated by ','.
    Ranges is the two numbers separated by '-'.
    Examples:
       declare -a ARRAY
       parse_range "1" ARRAY
       parse_range "1,7,9" ARRAY
       parse_range "0-7" ARRAY
       parse_range "1-3,6,7" ARRAY
       parse_range "0,2-7,9" ARRAY
    
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- base.sh
+++ base.sh
@@ -236,3 +236,74 @@ function find_extentions_in_dir( )
 }
 
 
+
+# This function parses list of numbers and ranges of numbers passed in the string
+# and fills an array of numbers passed as a second argument.
+# List of numbers and ranges must be passed as a string and separated by ','.
+# Ranges is the two numbers separated by '-'.
+# Examples:
+#     declare -a ARRAY
+#     parse_range "1" ARRAY
+#     parse_range "1,7,9" ARRAY
+#     parse_range "0-7" ARRAY
+#     parse_range "1-3,6,7" ARRAY
+#     parse_range "0,2-7,9" ARRAY
+function parse_range( )
+{
+   local INPUT_STRING="${1}"
+   local -n _PARSE_RANGE_RESULT_ARRAY_=${2}
+
+   _PARSE_RANGE_RESULT_ARRAY_=( )
+
+   IFS=',' read -r -a tokens <<< "${INPUT_STRING}"
+
+   for token in "${tokens[@]}"; do
+      if [[ "${token}" =~ ^[0-9]+$ ]]; then
+         _PARSE_RANGE_RESULT_ARRAY_+=("${token}")
+      elif [[ "${token}" =~ ^([0-9]+)-([0-9]+)$ ]]; then
+         start=${BASH_REMATCH[1]}
+         end=${BASH_REMATCH[2]}
+         if (( start <= end )); then
+            for (( i=start; i<=end; i++ )); do
+               _PARSE_RANGE_RESULT_ARRAY_+=("${i}")
+            done
+         else
+            print_error "Error: invalid range '${token}' (end is less then begin)"
+            exit 2
+         fi
+      else
+            print_error "Error: invalid '${token}'"
+            exit 2
+      fi
+   done
+}
+
+function test_parse_range( )
+{
+   declare -a ARRAY
+
+   local STRING="1"
+   parse_range ${STRING} ARRAY
+   print_info "'${STRING}' ->"
+   print_array ARRAY
+
+   local STRING="1,7,9"
+   parse_range ${STRING} ARRAY
+   print_info "'${STRING}' ->"
+   print_array ARRAY
+
+   local STRING="0-7"
+   parse_range ${STRING} ARRAY
+   print_info "'${STRING}' ->"
+   print_array ARRAY
+
+   local STRING="1-3,6,7"
+   parse_range ${STRING} ARRAY
+   print_info "'${STRING}' ->"
+   print_array ARRAY
+
+   local STRING="0,2-7,9"
+   parse_range ${STRING} ARRAY
+   print_info "'${STRING}' ->"
+   print_array ARRAY
+}
